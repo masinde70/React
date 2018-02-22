7b0abde4f7979609db497df30407e9d74a2b01c4
masinde70 <masinde.masinde@metropolia.fi>
2018-02-22 17:34:20 +0200

    my first app

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Background from './Background';
+
+class App extends Component {
+constructor(){
+	super();
+}
+
+  render() {
+    return (
+      <Background/>
+    )
+  }
+}
+
+export default App;
